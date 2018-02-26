637cf725ffe3d0d0c42a49caea0bc7b0f231bb6f
tianyuningmou <tianyuningmou2009@126.com>
2018-02-26 14:13:11 +0800

    IO文件操作

--- 14_IO文件操作.py
+++ 14_IO文件操作.py
@@ -17,4 +17,13 @@ CHANGE:  :
 MODIFIED: : @Time : 2018/2/26 下午2:04
 """
 
+"""
+    ndarray对象可以保存到磁盘文件并从磁盘文件加载；可用的IO功能有：
+        load()和save()函数处理numPy二进制文件(带npy扩展名)
+        loadtxt()和savetxt()函数处理正常的文本文件
+"""
 
+import numpy as np
+a = np.array([1,2,3,4,5])
+b = np.load('outfile.npy')
+print(b)
